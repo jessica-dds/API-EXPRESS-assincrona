f07567d3e1e26515d8128794463b55465784d8e7
jessica-dds <jessica.gleizer@hotmail.com>
2024-03-01 19:23:16 -0300

    primise all

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ const { getCityFromZipcode } = require('utils-playground');
 const app = express();
 
 app.get('/', async (req, res) => {
-    const cidade = await getCityFromZipcode('41256250');
+    const cidade = getCityFromZipcode('444380000');
+    const cidade2 = getCityFromZipcode('41256250');
 
-    res.send(`A cidade encontrada foi: ${cidade}`);
+    const promise = await Promise.all([cidade, cidade2]);
+
+    //console.log(promise);
+
+    const [resposta1, resposta2] = promise
+
+    res.send(`A cidade encontrada foi: ${resposta1} e ${resposta2}`);
 });
 
 app.listen(3000);
